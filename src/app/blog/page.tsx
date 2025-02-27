ce33e89d254437ebf3564f42271fc144763c0b83
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-02-27 17:00:30 +0200

    added loading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,5 @@
 export default function Blog() {
-  return <h1>Blog</h1>;
+	return <h1>Blog</h1>;
+	//  await new Promise((resolve) => setTimeout(resolve, 3000));
+  //  return <h1>Blog Page</h1>;
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
+
 export default function ProductsList() {
-	return (
-		<>
-			<h1>Product list</h1>
-			<h2>Product 1</h2>
-			<h2>Product 2</h2>
-			<h2>Product 3</h2>
-		</>
-	)
+	const productId = 100;
+
+  return (
+    <>
+      <h1>Product list</h1>
+      <h2>
+        <Link href='products/1'>Product 1</Link>
+      </h2>
+
+      <h2>
+        <Link href='products/2'>Product 2</Link>
+      </h2>
+      <h2>
+        <Link href='products/3'>Product 3</Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
+    </>
+  );
 }
